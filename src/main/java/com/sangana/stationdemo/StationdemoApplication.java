92f93e87c71b7135ec3437b16a94b77ff882890d
sangana <sanganas@us.ibm.com>
2018-08-04 16:30:00 -0500

    Initial checkin of the project

---
+++ src/main/java/com/sangana/stationdemo/StationdemoApplication.java
@@ -0,0 +1,12 @@
+package com.sangana.stationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StationdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StationdemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sangana/stationdemo/StationdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sangana.stationdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StationdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
