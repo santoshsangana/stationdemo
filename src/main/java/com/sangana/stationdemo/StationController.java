f19eab66ee3c3efd98d62f029c1daf9ed1357f74
sangana <sanganas@us.ibm.com>
2018-08-06 15:37:33 -0500

    more changes

--- src/main/java/com/sangana/stationdemo/StationController.java
+++ src/main/java/com/sangana/stationdemo/StationController.java
@@ -4,96 +4,82 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-
-import javax.persistence.EntityManager;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.QueryMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sangana.stationdemo.dao.StationDAO;
 import com.sangana.stationdemo.domain.Station;
 
-
 /**
  * @author sksangana
  *
  */
 @RestController
+@RequestMapping("/station")
 public class StationController {
-	
+
 	@Autowired
 	StationDAO stationDao;
-	
-@GetMapping("/{id}")	
-public Optional<Station> findStationById(@PathVariable String id) {
-	
-	return stationDao.findById(id);
-}
 
-//@GetMapping("/{name}")	
-@RequestMapping(method = RequestMethod.GET)
-public @ResponseBody List<Station> findStationByName(@RequestParam(value="name") String name) {
-	
-	//model.put("tag1", val1);
-	List<Station>  stations = stationDao.findAll();
-	List<Station> reponseList = new ArrayList<>();
-	
-	for(Station station : stations) {
-		if(station.getName().equalsIgnoreCase(name)) {
-			reponseList.add(station);
-		}
+	@GetMapping("/{id}")
+	public Optional<Station> findStationById(@PathVariable String id) {
+
+		return stationDao.findById(id);
 	}
-	return reponseList;
-}
 
-@RequestMapping(value="/hdEnabled/{isHdEnabled}", method = RequestMethod.GET)
-public @ResponseBody List<Station> findStationByhdEnabled(@PathVariable String isHdEnabled) {
-	
-	//model.put("tag1", val1);
-	List<Station>  stations = stationDao.findAll();
-	List<Station> reponseList = new ArrayList<>();
-	
-	for(Station station : stations) {
-		if(station.isHdEnabled() && isHdEnabled.equalsIgnoreCase("true")) {
-			reponseList.add(station);
-		}
+	@GetMapping("/name/{name}")
+	public List<Station> findStationByName(@PathVariable String name) {
+
+		List<Station> stations = stationDao.findAll();
+		List<Station> responseList = new ArrayList<>();
+
+		responseList = stations.stream().filter(station -> station.getName().equalsIgnoreCase(name)).collect(Collectors.toList());
+		return responseList;
 	}
-	return reponseList;
-}
 
-@GetMapping("/all")	
-public @ResponseBody List<Station> findAllStations(Map model) {
-	
-	//model.put("tag1", val1);
-	
-	return stationDao.findAll();
-}
+	@GetMapping("/hdenabled/{isHdEnabled}")
+	public List<Station> findStationByhdEnabled(@PathVariable String isHdEnabled) {
 
-@DeleteMapping("/delete/{id}")
-public void deleteStudent(@PathVariable String id) {
-	stationDao.deleteById(id);
-}
+		List<Station> stations = stationDao.findAll();
+		List<Station> result = new ArrayList<>();
 
-@PostMapping("/create")
-public @ResponseBody Station createStation(@RequestBody Station station) {
-	Station savedStation = stationDao.save(station);
+		boolean filter;
+		if (isHdEnabled.equalsIgnoreCase("true"))
+			filter = true;
+		else
+			filter = false;
 
-//	URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-//			.buildAndExpand(savedStudent.getId()).toUri();
+		result = stations.stream().filter(x -> x.isHdEnabled() == filter).collect(Collectors.toList());
 
-	return savedStation;
+		return result;
+	}
 
-}
+	@GetMapping("/all")
+	public List<Station> findAllStations(Map model) {
+
+		return stationDao.findAll();
+	}
+
+	@DeleteMapping("/delete/{id}")
+	public void deleteStudent(@PathVariable String id) {
+		stationDao.deleteById(id);
+	}
+
+	@PostMapping("/create")
+	public Station createStation(@RequestBody Station station) {
+		Station savedStation = stationDao.save(station);
+
+		return savedStation;
+
+	}
 
 }
