25cefe3c475ccc4a64c903022f79f9e9ccd2121c
sangana <sanganas@us.ibm.com>
2018-08-06 00:43:23 -0500

    adding the DAO class

---
+++ src/main/java/com/sangana/stationdemo/dao/StationDAO.java
@@ -0,0 +1,25 @@
+package com.sangana.stationdemo.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+
+import org.springframework.data.domain.Example;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.sangana.stationdemo.domain.Station;
+
+@RepositoryRestResource
+public interface StationDAO extends CrudRepository<Station, String> {
+	
+	List<Station> findAll();
+	Optional<Station> findById(String id);
+	List<Station> findStationByName(String name);
+	@SuppressWarnings("unchecked")
+	Station save(Station s); 
+	void deleteById(String id);
+	List<Station> findStationsByhdEnabled(String hdenabled);
+}
